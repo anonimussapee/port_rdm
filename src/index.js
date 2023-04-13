b2ee02cfd9d1d3ee46270301d1496a456c96b037
jdx <librededrogas420@gmail.com>
2023-04-13 15:06:55 -0500

    solo hice el npm run build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Template from '@templates/Template.js';
 import '@styles/main.css';
-console.log("hello world");
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
